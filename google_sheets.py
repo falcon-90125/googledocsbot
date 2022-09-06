5809e7c71c291c7573329ad2e1ac53c636918080
falcon-90125 <sokolov_aa@ngs.ru>
2022-09-06 20:01:33 +0700

    ''

--- google_sheets.py
+++ google_sheets.py
@@ -1,13 +1,18 @@
 import gspread
 from settings import CREDENTIALS, SHEET_ID
 
-SERVICE = gspread.service_account_from_dict(CREDENTIALS)
+#Это ГЛАВНЫЙ ОБЪЕКТ API - получает доступ к документу, может открыть существующий документ, создать новый и т.д.
+SERVICE = gspread.service_account_from_dict(CREDENTIALS) #Для инициализации сервиса (Google Sheets) из словаря, CREDENTIALS из файла settings
+
+# Далее создадим константу SHEET - это будет сам документ. У объекта SERVICE есть функция open_by_key передаем в нее ID документа из его адресной строки, это SHEET_ID из файла settings
 SHEET = SERVICE.open_by_key(SHEET_ID)
+
+#Далее нам нужно получить WORKSHEET, т.е. лист документа. Создадим константу WORKSHEET, в которую передаем первый лист документа. Функция get_worksheet получает лист по индексу:
+# 0 - это индекс первого листа документа
 WORKSHEET = SHEET.get_worksheet(0)
 
-values_list = [1, 'abc', 3]
-def get_numeric_list(input_list):
-    output_list = []
+def get_numeric_list(input_list): #Функция, которая преобразует все значения по колонке "Выручка" к числовым
+    output_list = []             # В input_list будет подаваться values_list в функции get_total_revenue, см. ниже
     for i in input_list:
         try:
             output_list.append(float(i))
@@ -15,9 +20,11 @@ def get_numeric_list(input_list):
             pass
     return output_list
 
-def get_total_revenue():
-    values_list = WORKSHEET.col_values(3, 'UNFORMATTED_VALUE')
-    return sum(get_numeric_list(values_list))
+def get_total_revenue(): #Функция, которая считает сумму всех значений списка values_list - по колонке "Выручка"
+    values_list = WORKSHEET.col_values(3, 'UNFORMATTED_VALUE') #передаёт в список значения по колонке "Выручка" с листа WORKSHEET
+    # col_values - функция возвращает все данные из 3й колоки таблицы realty в Google Doc и включает в себя функцию get_numeric_list
+    # параметр UNFORMATTED_VALUE значение будет вычислено как неотформатированное, т.к. считает все форматы (см.Ctrl+клик > описание с сылкой на документацию), преобразовав все значения к числовому типу.
+    return sum(get_numeric_list(values_list)) #суммирует преобразованные в числа функцией get_numeric_list значения списка
 
-def add_row(values_list):
+def add_row(values_list): #функция по добавлению данных в Google Doc
     WORKSHEET.insert_row(values_list, 2)
--- tg_bot.py
+++ tg_bot.py
@@ -5,11 +5,12 @@ from google_sheets import get_total_revenue, add_row
 from settings import TG_TOKEN, SHEET_URL
 from utils import create_batton
 
-bot = telebot.TeleBot(TG_TOKEN, parse_mode=None)
+bot = telebot.TeleBot(TG_TOKEN, parse_mode=None) #TG_TOKEN из файла settings
 
 BTN_DOC = create_batton('doc', '📝 Показать документ')
 BTN_REV = create_batton('rev','💲 Общая выручка')
 BTN_NEW_ITEM = create_batton('new_item', '✅ Добавить продажу')
+BTN_CANCEL = create_batton('cancel', '❌ Отмена')
 
 def add_new_item(message):
     new_item = [message.from_user.first_name]
@@ -26,30 +27,59 @@ def add_revenue(message, **kwargs):
     new_item = kwargs.get('new_item', [])
     new_item.append(message.text)
     add_row(new_item)
-    bot.send_message(message.chat.id, 'Спасибо 👍. Я добавил в отчет новую продажу.')
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    btn1 = types.InlineKeyboardButton(BTN_DOC['text'], callback_data=BTN_DOC['key'])
+    btn2 = types.InlineKeyboardButton(BTN_REV['text'], callback_data=BTN_REV['key'])
+    btn3 = types.InlineKeyboardButton(BTN_CANCEL['text'], callback_data=BTN_CANCEL['key'])
+    markup.add(btn1, btn2, btn3)
+
+    bot.send_message(message.chat.id, 'Спасибо 👍. Я добавил в отчет новую продажу.',
+                     reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        if call.data == BTN_DOC['key']:
+            bot.send_message(call.message.chat.id,
+                             f'<a href="{SHEET_URL}">Отчёт по продажам</a>',
+                             parse_mode='html')
+        elif call.data == BTN_REV['key']:
+            bot.send_message(call.message.chat.id,
+                             f'Общая выручка = <b>{get_total_revenue()}</b>',
+                             parse_mode='html')
+        elif call.data == BTN_CANCEL['key']:
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.message_id,
+                                  text="OK 👌",
+                                  reply_markup=None)
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    img = open('start.jpg', 'rb')
-    bot.send_sticker(message.chat.id, img)
+def send_welcome(message):  #Функция приветственного сообщения
+    img = open('start.jpg', 'rb') #приветственная картинка
+    bot.send_sticker(message.chat.id, img) #выводит приветственную картинку
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton(BTN_DOC['text'])
     btn2 = types.KeyboardButton(BTN_REV['text'])
     btn3 = types.KeyboardButton(BTN_NEW_ITEM['text'])
     markup.add(btn1, btn2, btn3)
+#Метод reply_to отвечает на сообщение. Мы предаем 2 параметра в этот метод: сообщение отправленное пользователем, на которое нужно ответить и текст нашего сообщения:
+    bot.reply_to(message, "Привет! Это Ваш личный помошник. Чего изволите?", reply_markup=markup)
 
-    bot.reply_to(message, "Привет! Это твой личный помошник. Чего изволите?", reply_markup=markup)
-
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
+#Функция, которая реагирует на нажатие кнопок и передаёт ответы по условиям
+@bot.message_handler(content_types=['text']) #оборачиваем функцию send_text в декоратор message_handler, но в данном случае мы передаем параметр content_types, который содержит тип текст.
+# Это значит, что наша собственная функция send_text будет срабатывать, когда в бот будет отправлено любое текстовое сообщение.
+def send_text(message): #Функция реагирует на нажатие кнопок
+#В теле самой функции мы проверяем чему равен текст сообщения - какая нажата кнопка?
+# Если он равен '📝 Показать документ', то вызываем метод бота send_message. Метод send_message отправляет сообщение.
+# Первый параметр метода - ID чата. Второй параметр - текст самого сообщения. Третий параметр parse_mode (опциональный) - режим парсинга сообщения
     if message.text == BTN_DOC['text']:
-        bot.send_message(message.chat.id, f'<a href="{SHEET_URL}">Отчёт по продажам</a>',
+        bot.send_message(message.chat.id, f'<a href="{SHEET_URL}">Отчёт по продажам</a>', #Выводит ссылку на файл-таблицу в Google Sheet
                          parse_mode='html')
     elif message.text == BTN_REV['text']:
         bot.send_message(message.chat.id,
-                         f'Общая выручка = <b>{get_total_revenue()}</b>',
+                         f'Общая выручка = <b>{get_total_revenue()}</b>', #Выводит сумму Общая выручка по функции get_total_revenue
                          parse_mode='html')
     elif message.text == BTN_NEW_ITEM['text']:
         add_new_item(message)
